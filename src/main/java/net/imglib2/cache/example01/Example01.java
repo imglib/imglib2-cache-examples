4c70f2bd4baa867131cd55ab7f1fe073f40cd9d2
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2017-03-16 20:53:38 -0400

    initial basic example

---
+++ src/main/java/net/imglib2/cache/example01/Example01.java
@@ -0,0 +1,70 @@
+package net.imglib2.cache.example01;
+
+import java.io.IOException;
+
+import org.scijava.ui.behaviour.DragBehaviour;
+import org.scijava.ui.behaviour.io.InputTriggerConfig;
+import org.scijava.ui.behaviour.util.Behaviours;
+
+import bdv.util.Bdv;
+import bdv.util.BdvFunctions;
+import bdv.viewer.ViewerPanel;
+import net.imglib2.RandomAccess;
+import net.imglib2.RealPositionable;
+import net.imglib2.algorithm.neighborhood.HyperSphereShape;
+import net.imglib2.algorithm.neighborhood.Neighborhood;
+import net.imglib2.cache.img.DiskCachedCellImgFactory;
+import net.imglib2.img.Img;
+import net.imglib2.position.transform.Round;
+import net.imglib2.type.numeric.ARGBType;
+import net.imglib2.view.Views;
+
+public class Example01
+{
+	public static void main( final String[] args ) throws IOException
+	{
+		final long[] dimensions = new long[] { 640, 640, 640 };
+
+		final Img< ARGBType > img = new DiskCachedCellImgFactory< ARGBType >()
+				.create( dimensions, new ARGBType() );
+
+		final Bdv bdv = BdvFunctions.show( img, "DiskCachedCellImg" );
+
+
+		/*
+		 * Install behaviour for painting into img with shortcut "D"
+		 */
+
+		final Behaviours behaviours = new Behaviours( new InputTriggerConfig() );
+		behaviours.install( bdv.getBdvHandle().getTriggerbindings(), "paint" );
+		behaviours.behaviour( new DragBehaviour()
+		{
+			final ViewerPanel viewer = bdv.getBdvHandle().getViewerPanel();
+			final RandomAccess< Neighborhood< ARGBType > > sphere = new HyperSphereShape( 10 ).neighborhoodsRandomAccessible( Views.extendZero( img ) ).randomAccess();
+			final RealPositionable roundpos = new Round<>( sphere );
+
+			void draw( final int x, final int y )
+			{
+				viewer.displayToGlobalCoordinates( x, y, roundpos );
+				sphere.get().forEach( t -> t.set( 0xFFFF0000 ) );
+				viewer.requestRepaint();
+			}
+
+			@Override
+			public void init( final int x, final int y )
+			{
+				draw( x, y );
+			}
+
+			@Override
+			public void end( final int x, final int y )
+			{}
+
+			@Override
+			public void drag( final int x, final int y )
+			{
+				draw( x, y );
+			}
+		}, "paint", "D" );
+	}
+}
