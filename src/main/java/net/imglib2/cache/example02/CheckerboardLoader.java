7532466ef802a6dc77100df87880b03b740dcaf2
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2017-03-16 21:10:26 -0400

    Example02: options and custom cell loader

---
+++ src/main/java/net/imglib2/cache/example02/CheckerboardLoader.java
@@ -0,0 +1,48 @@
+package net.imglib2.cache.example02;
+
+import java.util.Arrays;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import net.imglib2.cache.CacheLoader;
+import net.imglib2.img.basictypeaccess.array.DirtyIntArray;
+import net.imglib2.img.cell.Cell;
+import net.imglib2.img.cell.CellGrid;
+import net.imglib2.util.Intervals;
+
+public class CheckerboardLoader implements CacheLoader< Long, Cell< DirtyIntArray > >
+{
+	private final CellGrid grid;
+
+	ConcurrentHashMap< Long, AtomicInteger > loadcounts = new ConcurrentHashMap<>();
+
+	AtomicInteger loadcount = new AtomicInteger( 0 );
+
+	public CheckerboardLoader( final CellGrid grid )
+	{
+		this.grid = grid;
+	}
+
+	@Override
+	public Cell< DirtyIntArray > get( final Long key ) throws Exception
+	{
+		final long index = key;
+
+		final int n = grid.numDimensions();
+		final long[] cellMin = new long[ n ];
+		final int[] cellDims = new int[ n ];
+		grid.getCellDimensions( index, cellMin, cellDims );
+		final int blocksize = ( int ) Intervals.numElements( cellDims );
+		final DirtyIntArray array = new DirtyIntArray( blocksize );
+
+		final long[] cellGridPosition = new long[ n ];
+		grid.getCellGridPositionFlat( index, cellGridPosition );
+		long sum = 0;
+		for ( int d = 0; d < n; ++d )
+			sum += cellGridPosition[ d ];
+		final int color = ( sum % 2 == 0 ) ? 0xff000000 : 0xff008800;
+		Arrays.fill( array.getCurrentStorageArray(), color );
+
+		return new Cell<>( cellDims, cellMin, array );
+	}
+}
---
+++ src/main/java/net/imglib2/cache/example02/Example02.java
@@ -0,0 +1,91 @@
+package net.imglib2.cache.example02;
+
+import static net.imglib2.cache.img.DiskCachedCellImgOptions.options;
+
+import java.io.IOException;
+
+import org.scijava.ui.behaviour.DragBehaviour;
+import org.scijava.ui.behaviour.io.InputTriggerConfig;
+import org.scijava.ui.behaviour.util.Behaviours;
+
+import bdv.util.Bdv;
+import bdv.util.BdvFunctions;
+import bdv.viewer.ViewerPanel;
+import net.imglib2.RandomAccess;
+import net.imglib2.RealPositionable;
+import net.imglib2.algorithm.neighborhood.HyperSphereShape;
+import net.imglib2.algorithm.neighborhood.Neighborhood;
+import net.imglib2.cache.CacheLoader;
+import net.imglib2.cache.img.DiskCachedCellImgFactory;
+import net.imglib2.cache.img.DiskCachedCellImgOptions;
+import net.imglib2.cache.img.DiskCachedCellImgOptions.CacheType;
+import net.imglib2.img.Img;
+import net.imglib2.img.basictypeaccess.array.DirtyIntArray;
+import net.imglib2.img.cell.Cell;
+import net.imglib2.img.cell.CellGrid;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.position.transform.Round;
+import net.imglib2.type.numeric.ARGBType;
+import net.imglib2.view.Views;
+
+public class Example02
+{
+	public static void main( final String[] args ) throws IOException
+	{
+		final int[] cellDimensions = new int[] { 64, 64, 64 };
+		final long[] dimensions = new long[] { 640, 640, 640 };
+
+		final DiskCachedCellImgOptions options = options()
+				.cellDimensions( cellDimensions )
+				.cacheType( CacheType.BOUNDED )
+				.maxCacheSize( 100 );
+
+		final CacheLoader< Long, Cell< DirtyIntArray > > loader = new CheckerboardLoader( new CellGrid( dimensions, cellDimensions ) );
+
+		final Img< ARGBType > img = new DiskCachedCellImgFactory< ARGBType >( options ).create(
+				dimensions,
+				new ARGBType(),
+				loader );
+
+		ImageJFunctions.show( img );
+
+		final Bdv bdv = BdvFunctions.show( img, "Example02" );
+
+
+		/*
+		 * Install behaviour for painting into img with shortcut "D"
+		 */
+
+		final Behaviours behaviours = new Behaviours( new InputTriggerConfig() );
+		behaviours.install( bdv.getBdvHandle().getTriggerbindings(), "paint" );
+		behaviours.behaviour( new DragBehaviour()
+		{
+			final ViewerPanel viewer = bdv.getBdvHandle().getViewerPanel();
+			final RandomAccess< Neighborhood< ARGBType > > sphere = new HyperSphereShape( 10 ).neighborhoodsRandomAccessible( Views.extendZero( img ) ).randomAccess();
+			final RealPositionable roundpos = new Round<>( sphere );
+
+			void draw( final int x, final int y )
+			{
+				viewer.displayToGlobalCoordinates( x, y, roundpos );
+				sphere.get().forEach( t -> t.set( 0xFFFF0000 ) );
+				viewer.requestRepaint();
+			}
+
+			@Override
+			public void init( final int x, final int y )
+			{
+				draw( x, y );
+			}
+
+			@Override
+			public void end( final int x, final int y )
+			{}
+
+			@Override
+			public void drag( final int x, final int y )
+			{
+				draw( x, y );
+			}
+		}, "paint", "D" );
+	}
+}
